54f07f716dbee39e6f988ca20f0c8cbc01eedf63
wexnot <53924803+wexnot@users.noreply.github.com>
2019-11-25 20:42:46 -0500

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -1,6 +1,6 @@
 from random import randint as rt
 import math
-#import bpy
+import bpy
 
 class Fighter:
     """Fighter class that can calculate vectors and instanciate simulations"""
@@ -16,11 +16,11 @@ class Fighter:
         dx = x - mx
         dy = y - my
         dz = z - mz
-        print(dx, dy, dz)
+        #print(dx, dy, dz)
 
         d = math.sqrt(dx*dx + dy*dy + dz*dz)
         self.distance = d
-        print('distance', d)
+        #print('distance', d)
         self.horizontal = dx/d * self.missile_speed
         self.vertical = dy/d * self.missile_speed
         self.deph = dz/d * self.missile_speed
@@ -30,16 +30,22 @@ class Fighter:
         missile_x = 8
         missile_y = 8
         missile_z = 8
+        
+        movements = []
+        mposition = (missile_x, missile_y, missile_z)
 
         while True:
             self.radian_angle(missile_x, missile_y, missile_z, x, y, z)
+            mposition = (missile_x, missile_y, missile_z)
+            movements.append(mposition)
 
-            print('Fighter jet position is ({}, {}, {})'.format(x, y, z))
-            print('missile position is ({}, {}, {})'.format(missile_x, missile_y, missile_z))
-            print('x distance is {}, y is {} and z is {}'.format(abs(missile_x - x), abs(missile_y - y), abs(missile_z - z)))
+            #print('Fighter jet position is ({}, {}, {})'.format(x, y, z))
+            #print('missile position is ({}, {}, {})'.format(missile_x, missile_y, missile_z))
+            #print('x distance is {}, y is {} and z is {}'.format(abs(missile_x - x), abs(missile_y - y), abs(missile_z - z)))
 
             if self.distance < 1:
                 print('done didly done')
+                return movements
                 break
             else:
                 missile_x += self.horizontal
@@ -49,6 +55,14 @@ class Fighter:
 
 harrier = Fighter('Harrier royal navy', 'Harrier', True, False, 0)
 
-harrier.simulation()
+#harrier.simulation()
+jet = bpy.data.objects["Cube"]
+movements = harrier.simulation()
 
-#print(bpy.data)
+frame_num = 0
+
+for position in movements:
+    bpy.context.scene.frame_set(frame_num)
+    jet.location = position
+    jet.keyfram_insert(data_path="location", index = -1)
+    frame_num += 20
