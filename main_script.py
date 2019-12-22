928c9a8efc12cd659321178ebc664e701c193244
Avraham Figueroa <53924803+wexnot@users.noreply.github.com>
2019-12-22 16:41:27 -0500

    all calculations are now numpy a the missile has a timer

--- main_script.py
+++ main_script.py
@@ -1,92 +1,109 @@
-from random import randint
-import mathutils
 import math
-import bpy
+import mathutils
+# import bpy
+import random
+import numpy as np
 
 class Jet:
+
     jet_movements = []
-    x = 5
-    y = 5
-    z = 5
+    cartesian = np.array((5,5,5))
 
     def __init__(self, name, speed):
+        # Name does not affect the outcome
         self.name = name
+        # The initial speed is always smaller than the constant speed of the missile
         self.speed = speed
 
     def vector(self):
-        Jet.jet_movements.append((self.x, self.y, self.z))
-        self.x += self.speed
+        """
+        This is where the Jet receive commands the default
+        is a vector with constant speed machine learning can be inserted
+        """
+        Jet.jet_movements.append(self.cartesian)
+        self.cartesian[0] += self.speed
 
 class Missile:
+
     missile_movements = []
     missile_rotations = []
-    x = 8
-    y = 3
-    z = 0
+    time_left = 60
+    cartesian = np.array((8,3,0))
 
     def __init__(self, target):
+        # Target is a Jet or any moving object
         self.target = target
+        # Missile speed is slightly faster than the target
         self.speed = target.speed + 1
 
     def simulation(self):
+        """
+        This is where the missile will be simulated
+        """
         while True:
-            dx = self.target.x - self.x
-            dy = self.target.y - self.y
-            dz = self.target.z - self.z
 
-            self.missile_movements.append((self.x, self.y, self.z))
-            print("Jet location:", self.target.x, self.target.y)
-            print("Missile location:", self.x, self.y, self.target.z)
+            self.missile_movements.append(self.cartesian)
 
-            d = math.sqrt(dx*dx + dy*dy + dz*dz)
+            distance_matrix = self.target.cartesian - self.cartesian
+            d = math.sqrt(sum(distance_matrix ** 2))
 
-            xmove = dx/d * self.speed
-            ymove = dy/d * self.speed
-            zmove = dz/d * self.speed
-
-            jev = mathutils.Vector((self.target.x, self.target.y, self.target.z))
+            jev = mathutils.Vector((self.target.cartesian))
             jeq = jev.to_track_quat()
             jee = jeq.to_euler()
+
             self.missile_rotations.append(tuple(jee))
+
+            # print("distance matrix:", distance_matrix)
+            print("Jet location    :", self.target.cartesian)
+            print("Missile location:", self.cartesian.astype(int))
+            print("Time left:", self.time_left)
 
             if d < 1:
                 print("done didly done")
                 break
+
+            elif self.time_left <= 0:
+                print("The missile exploded before impact")
+                break
+
             else:
-                self.x += xmove
-                self.y += ymove
-                self.z += zmove
+                series = distance_matrix/d * self.speed
+                self.cartesian += series.astype(int)
                 self.target.vector()
+                self.time_left -= 1
                 continue
 
+# class Blender:
+#
+#     simissile = bpy.data.objects["Cube"]
+#     sim_jet = bpy.data.objects["Cone"]
+#
+#     @staticmethod
+#     def porting(point, matrices, set):
+#         frame_num = 0
+#
+#         for x in matrices:
+#
+#             if set == "location":
+#                 point.location = x
+#
+#             elif set == "rotation_euler":
+#                 point.rotation_euler = x
+#
+#             bpy.context.scene.frame_set(frame_num)
+#             point.keyframe_insert(data_path=set, index = -1)
+#             frame_num += 5
+#
+#         bpy.context.scene.frame_end = frame_num - 5
+
 harrier = Jet("Harrier", 1)
-harrier.x = 100
-harrier.y = 100
-harrier.z = 100
+harrier.x = 8
+harrier.y = 3
+harrier.z = 6
 
 dunning = Missile(harrier)
-
 dunning.simulation()
 
-simissile = bpy.data.objects["Cube"]
-sim_jet = bpy.data.objects["Cone"]
-
-def porting(point, matrices, cat=True):
-    frame_num = 0
-
-    for x in matrices:
-        bpy.context.scene.frame_set(frame_num)
-        if cat == True:
-            point.location = x
-            point.keyframe_insert(data_path="location", index = -1)
-        else:
-            point.rotation_euler = x
-            point.keyframe_insert(data_path="rotation_euler", index = -1)
-
-        frame_num += 5
-
-    bpy.context.scene.frame_end = frame_num - 5
-
-porting(simissile, dunning.missile_movements)
-porting(sim_jet, harrier.jet_movements)
-porting(simissile, dunning.missile_rotations, False)
+# Blender.porting(self.simissile, dunning.missile_movements, "location")
+# Blender.porting(self.sim_jet, harrier.jet_movements, "location")
+# Blender.porting(self.simissile, dunning.missile_rotations, "rotation_euler")
